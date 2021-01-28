8b7d57757b9b5b7b126e2a1b0b0da30413fa9f6f
Alex <aleksim.dev@gmail.com>
2021-01-28 19:40:41 +0200

    test alert window

--- src/screens/TodoScreen.js
+++ src/screens/TodoScreen.js
@@ -1,19 +1,44 @@
 import React from "react";
-import { StyleSheet, View, Text, Button } from "react-native";
+import { StyleSheet, View, Text, Button, Alert } from "react-native";
 
 export const TodoScreen = ({ todo, backToMainScreen, removeTodo }) => {
+  const deleteTodo = (todo) => {
+    removeTodo(todo.id);
+    backToMainScreen();
+  };
   return (
     <View>
-      <Text>{todo.title}</Text>
+      <Text style={styles.text}>{todo.title}</Text>
       <Button title="Back" onPress={backToMainScreen} />
       <Button
         title="Delete"
+        color="red"
         onPress={() => {
-          removeTodo(todo.id);
-          backToMainScreen();
+          Alert.alert(
+            "Are you sure?",
+            "If you delete the task you will never see it again.",
+            [
+              {
+                text: "Never ask me again",
+                onPress: () => console.log("never"),
+              },
+              {
+                text: "Cancel",
+                onPress: () => console.log("Cancel Pressed"),
+                style: "cancel",
+              },
+              { text: "OK", onPress: () => console.log("OK Pressed") },
+            ]
+          );
+          deleteTodo(todo);
         }}
       />
     </View>
   );
 };
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  text: {
+    marginBottom: 50,
+    fontSize: 20,
+  },
+});
